4abeff944a6fa00094062175ffceb0acd2aa9757
Andrew Yourtchenko <ayourtch@gmail.com>
2017-11-15 01:08:22 +0100

    make it work properly

--- src/main.rs
+++ src/main.rs
@@ -2,96 +2,46 @@ extern crate ssh2;
 
 use ssh2::{Session, Channel, Error};
 use std::net::{TcpStream};
+extern crate owning_ref;
 
-struct DeviceSSHConnection <'h> {
+use owning_ref::OwningHandle;
+
+struct DeviceSSHConnection {
   tcp: TcpStream,
-  session: Session,
-  target: String,
-  username: String,
-  password: Option<String>,
-  // channel: Result<Channel <'h>, Error>, // Option<Channel<'h>>,
-  channel_result: Result<Channel <'h>, Error>,
-  channel: Option<Channel<'h>>,
+  channel: OwningHandle<Box<Session>, Box<Channel<'static>>>,
 }
 
-
-impl <'h> DeviceSSHConnection <'h> {
-
-
-  fn start(&mut self) -> () {
-    println!("Starting!");
-    self.session.handshake(&self.tcp).unwrap();
-    self.session.set_timeout(5000);
-    match self.password {
-      Some(ref passwd) => { 
-        self.session.userauth_password(&self.username, passwd).unwrap();
-      },
-      None => { panic!("Missing password!"); },
-    };
-    self.password = None;
-
-  }
-
-  fn get_channel <'x>(dev: &mut DeviceSSHConnection<'x>) {
-    match (dev.session.channel_session()) {
-      Ok(c) => { dev.channel = Some(c) },
-      Err(e) => { dev.channel = None },
-    }
-  }
-
-  fn start2(&mut self) {
-    DeviceSSHConnection::get_channel(&mut self);
-
-    match self.channel {
-      Some(ref mut c) => {
-        let res = c.shell().unwrap();
-        println!("connection shell res = {:?}", res);
-      },
-      None => { panic!("No channel!"); },
-    }
-  }
-
-  fn new(targ: &'h str, c_user: &str, c_pass: &str) -> Self {
+impl DeviceSSHConnection {
+  fn new(targ: &str, c_user: &str, c_pass: &str) -> Self {
     use std::net::{TcpStream};
-    let tcp = TcpStream::connect(targ).unwrap();
+    let mut session = Session::new().unwrap();
+    let mut tcp = TcpStream::connect(targ).unwrap();
 
+    session.handshake(&tcp).unwrap();
+    session.set_timeout(5000);
+    println!("Authenticating...");
+    session.userauth_password(c_user, c_pass).unwrap();
 
-    let mut connection = DeviceSSHConnection {
+    let mut sess = Box::new(session);
+    let mut oref = OwningHandle::new_with_fn(sess, unsafe { |x| Box::new((*x).channel_session().unwrap()) } );
+    oref.shell().unwrap();
+    let ret = DeviceSSHConnection {
       tcp: tcp,
-      target: targ.to_string(),
-      username: c_user.to_string(),
-      password: Some(c_pass.to_string()),
-      session: Session::new().unwrap(),
-      channel: None,
-      channel_result: Err(Error::unknown()),
+      channel: oref
     };
-    connection
-  }
-  
-  fn log(&mut self, src: &str, lvl: i32, message: &str) {
+    ret 
   }
 
-  fn send(&mut self, data: &str) -> std::io::Result<usize> {
+  fn write(&mut self, data: &str) -> std::io::Result<usize> {
     use std::io::prelude::*;
-    match self.channel {
-      Some(ref mut c) => {
-        c.write(data.as_bytes())
-      },
-      None => { panic!("No channel!"); },
-    }
+    self.channel.write(data.as_bytes())
   }
   fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
     use std::io::prelude::*;
-    match self.channel {
-      Some(ref mut c) => {
-        c.read(buf)
-      },
-      None => { panic!("No channel!"); },
-    }
+    self.channel.read(buf)
   }
 
   fn do_io(&mut self) {
-    // use std::io::prelude::*;
     loop {
       let mut buf = [1u8; 16000];
       let res = self.read(&mut buf);
@@ -102,28 +52,24 @@ impl <'h> DeviceSSHConnection <'h> {
   }
 
   fn finish(&mut self) {
-    //use std::io::prelude::*;
-    match self.channel {
-      Some(ref mut c) => {
-        c.wait_close();
-        println!("{}", c.exit_status().unwrap());
-      },
-      None => { panic!("No channel!"); },
-    }
+    self.channel.wait_close();
+    println!("{}", self.channel.exit_status().unwrap());
   }
-
 }
 
 
+fn conn() -> OwningHandle<Box<Session>, Box<Channel<'static>>> {
+  let sess = Box::new(Session::new().unwrap());
+  let oref = OwningHandle::new_with_fn(sess, unsafe { |x| Box::new((*x).channel_session().unwrap()) } );
+  oref
+}
+
 fn main() {
   println!("hello!");
-
   let target = "192.168.127.151:22";
-  let mut conn = DeviceSSHConnection::new(&target, "user", "pass");
-  conn.start();
-  conn.start2();
-  conn.send("term len 0\n");
-  conn.send("show version\n");
-  conn.do_io();
-  conn.finish();
+
+  let mut s2 = DeviceSSHConnection::new(&target, "aytest", "cisco123");
+  s2.write("term len 0\n");
+  s2.write("show version\n");
+  s2.do_io();
 }
